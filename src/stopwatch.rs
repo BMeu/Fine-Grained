17c1aeafa40eb4117db9d5fbfb37c1aea10aa9ce
BMeu <bastian@bastianmeyer.eu>
2017-05-06 17:38:31 +0200

    Add module documentation

--- src/stopwatch.rs
+++ src/stopwatch.rs
@@ -4,6 +4,8 @@
 // MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option. This file may not be copied,
 // modified, or distributed except according to those terms.
 
+//! The actual stopwatch implementation.
+
 use std::fmt;
 
 use time;
