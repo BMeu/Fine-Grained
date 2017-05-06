ab42994f1a0a2ff507a8f4605d74823feec560c7
BMeu <bastian@bastianmeyer.eu>
2017-05-06 15:50:14 +0200

    Re-license under MIT and Apache

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// Copyright 2017 Bastian Meyer
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or http://apache.org/licenses/LICENSE-2.0> or the
+// MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms.
+
 //! A stopwatch with lap functionality and nanosecond resolution to time things.
 //!
 //! Measured times are stored and returned in nanoseconds.
